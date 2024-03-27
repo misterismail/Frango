ca94fccfbda574dd002b751125af3d4cede39426
misterismail <mahmoud.ahmadismail@gmail.com>
2024-03-27 09:46:34 -0300

    Implementamos o JavaScript e atualizamos o CSS.
    Co-authored-by: Renan Pepineli Mendes <renan-pepineli-mendes@users.noreply.github.com>

---
+++ assets/js/sript.js
@@ -0,0 +1,20 @@
+//Tratamento de eventos
+document.querySelector("#mail").addEventListener("change", registrar)
+document.querySelector("#botao-cadastrar").addEventListener("click", cadastrar)
+
+function registrar(){
+    const email_frango = document.querySelector("#mail").value
+    let email_resposta = console.log(email_frango)
+
+    
+}
+
+function cadastrar(){
+
+    alert("OBRIGADO PELO SEU EMAIL! <3")
+}
+
+
+
+//DOM - Document Object Model
+
